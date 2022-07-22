047b81e9480bc0070839cdc2c290befe48cb2997
shion1110 <shionn1110@gmail.com>
2022-07-22 15:32:14 +0900

    itemモデルカラム追加

--- db/migrate/20220722060451_create_items.rb
+++ db/migrate/20220722060451_create_items.rb
@@ -1,7 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string     :item_name,        null: false
+      t.text       :item_version,    null: false
+      t.integer    :category_id,     null: false
+      t.integer    :item_status_id, null: false
+      t.integer    :delivary_charge_id,  null: false
+      t.integer    :consignor_area_id,    null: false
+      t.integer    :days_ship_id,    null: false
+      t.integer    :cost,   null: false
+      t.references :user,   null: false, foreign_key: true 
       t.timestamps
     end
   end
