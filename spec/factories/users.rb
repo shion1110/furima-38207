551976d3f9b63896d165123217d94e30907163b5
shion1110 <shionn1110@gmail.com>
2022-07-22 14:25:04 +0900

    テストコード修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname { '田中' }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              {'abc1234'}
     password_confirmation { password }
     family_kanji { '山田' }
     first_kanji { '太郎' }
