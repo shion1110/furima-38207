239ccbc2f7a56c7d1d7ae949320cbd3ecc671b07
shion1110 <shionn1110@gmail.com>
2022-07-26 17:58:04 +0900

    発送の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user! ,except:[:index]
-  before_action :set_item, only: [:new]
+ 
 
   def index
     @items = Item.order(created_at: :DESC)
@@ -26,7 +26,5 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image,:item_name, :item_version, :category_id, :item_status_id,:delivary_charge_id, :consignor_area_id, :days_ship_id, :cost).merge(user_id: current_user.id)
   end
 
-  def set_tweet
-    @item = Item.find(params[:id])
-  end
+ 
 end 
