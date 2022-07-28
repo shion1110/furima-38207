a927d7caa02ae75788e336cdb70e328ec034e58c
shion1110 <shionn1110@gmail.com>
2022-07-28 19:18:22 +0900

    編集機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,13 +23,10 @@ class ItemsController < ApplicationController
   def show
   end
 
-  def edit
-    @item = Item.find(params[:id])
-    redirect_to root_path unless current_user == @item.user
+  def edit 
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
